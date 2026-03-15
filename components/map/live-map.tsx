6996d605bd0375214dd99dcbf9ed9ca57edf003b
oluwasetemi ojo <setemiojo@gmail.com>
2026-03-15 08:45:13 -0500

    chore: update the gitignore and revert live map

--- components/map/live-map.tsx
+++ components/map/live-map.tsx
@@ -50,10 +50,8 @@ export function LiveMap({ userLat, userLng, authorityMode }: LiveMapProps) {
       zoomControl={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        subdomains="abcd"
-        maxZoom={20}
+        attribution='&copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {(userLat && userLng) && <MapRecenter lat={userLat} lng={userLng} />}
       {/* Non-crime incidents: always show as pins */}
--- components/report/report-pin-map.tsx
+++ components/report/report-pin-map.tsx
@@ -43,10 +43,8 @@ export function ReportPinMap({ lat, lng, severity }: ReportPinMapProps) {
       dragging={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        subdomains="abcd"
-        maxZoom={20}
+        attribution='&copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <CircleMarker
         center={[lat, lng]}
